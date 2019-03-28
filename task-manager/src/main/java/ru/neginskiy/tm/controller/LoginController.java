bcbbb1d697c04dc98dd6cc81c56d303a5f76660d
nmice <n.mice@mail.ru>
2019-03-28 11:29:29 +0300

    added LoginController

--- task-manager/src/main/java/ru/neginskiy/tm/controller/LoginController.java
+++ task-manager/src/main/java/ru/neginskiy/tm/controller/LoginController.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.neginskiy.tm.api.service.IUserService;
 import ru.neginskiy.tm.entity.User;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import javax.servlet.http.*;
 
 @Controller
 public class LoginController {
@@ -26,102 +20,34 @@ public class LoginController {
         return "login";
     }
 
-    /*@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model*/
-    /*        model.addAttribute("name",name);*/
-
     @PostMapping("/login")
     public String authorization(
             @RequestParam("username") final String username,
             @RequestParam("name") final String name,
             @RequestParam("password") final String password,
-            
             HttpServletResponse httpServletResponse,
-            Model model
-    ) {
-        String username = username;
-        String name = name;
-        String password = password;
-
-        try {
-            final String passwordHash = passwordHashUtil.md5(password);
-            final String token = userService.signIn(username, passwordHash);
-            httpServletResponse.addCookie(new Cookie("token", token));
-            return "redirect:/project-list";
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
+            Model model) {
+        if (username == null) {
+            //is a create user page:
+            if (userService.isRegistredLogin(name)) {
+                model.addAttribute("loginMessage", "This login is registred now!");
+                return "login";
+            }
+            User user = new User();
+            user.setLogin(name);
+            user.setPassword(password);
+            userService.merge(user);
+            model.addAttribute("loginMessage", "Please login");
             return "login";
-        }
-
-
-
-
-
-        if (username == null) {
-            //is a create user page:
-            if (userService.isRegistredLogin(name)) {
-                req.setAttribute("loginMessage", "This login is registred now!");
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-                requestDispatcher.forward(req, resp);
-            }
-            User user = new User();
-            user.setLogin(name);
-            user.setPassword(password);
-            userService.merge(user);
-            req.setAttribute("loginMessage", "Please login");
-            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-            requestDispatcher.forward(req, resp);
         } else {
             //is a login page:
             User user = userService.findUser(username, password);
             if (user == null) {
-                req.setAttribute("loginMessage", "User not found!");
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-                requestDispatcher.forward(req, resp);
+                model.addAttribute("loginMessage", "User not found!");
+                return "login";
             } else {
-                HttpSession session = req.getSession();
-                session.setAttribute("userId", user.getId());
-                resp.sendRedirect(req.getContextPath() + "/project-list");
-            }
-        }
-
-
-
-
-    }
-
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String username = req.getParameter("username");
-        String name = req.getParameter("name");
-        String password = req.getParameter("password");
-
-        if (username == null) {
-            //is a create user page:
-            if (userService.isRegistredLogin(name)) {
-                req.setAttribute("loginMessage", "This login is registred now!");
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-                requestDispatcher.forward(req, resp);
-            }
-            User user = new User();
-            user.setLogin(name);
-            user.setPassword(password);
-            userService.merge(user);
-            req.setAttribute("loginMessage", "Please login");
-            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-            requestDispatcher.forward(req, resp);
-        } else {
-            //is a login page:
-            User user = userService.findUser(username, password);
-            if (user == null) {
-                req.setAttribute("loginMessage", "User not found!");
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-                requestDispatcher.forward(req, resp);
-            } else {
-                HttpSession session = req.getSession();
-                session.setAttribute("userId", user.getId());
-                resp.sendRedirect(req.getContextPath() + "/project-list");
+                httpServletResponse.addCookie(new Cookie("userId", user.getId()));
+                return "redirect:/project-list";
             }
         }
     }
